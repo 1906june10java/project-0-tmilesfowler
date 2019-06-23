05263fb93e85e77281d9a693db620616a2ee01cf
Timothy Miles Fowler <tmilesfowler@gmail.com>
2019-06-23 18:07:48 -0400

    Established two test cases for the .getUserInfo function. Next: cases for connection.

--- src/test/java/com/revature/tests/UnitTests.java
+++ src/test/java/com/revature/tests/UnitTests.java
@@ -1,9 +1,39 @@
 package com.revature.tests;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import com.revature.service.ServiceBridge;
 
 public class UnitTests {
-
 	
+	private static final ServiceBridge srvBrg = new ServiceBridge();
+	
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+	
+	//Login tests
+	@Test
+	public void infoGet() {
+		final String userTst = "revature";
+		final String [] expected = {"1", "REVATURE", "P4SSW0RD", "500"};
+		assertArrayEquals(expected, srvBrg.getUserInfo(userTst));
+		
+	}
+	
+	@Test
+	public void infoGetInvalid() {
+		final String userTst = "NOTrevature";
+		final String [] expected = {null, null, null, null};
+		assertArrayEquals(expected, srvBrg.getUserInfo(userTst));
+	}
 
+	//Balance change tests
+	
 }
